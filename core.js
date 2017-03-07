a00697eb28404c51d6ef0209110c2d41916b5287
Matthew Fors <matthew.fors@cenx.com>
2017-03-07 18:43:33 -0500

    New Commit

--- core.js
+++ core.js
@@ -23,40 +23,59 @@
 
 var snoowrap = require('snoowrap');
 var credentials = require('./credentials.js');
+var fs = require('fs'); // File System
+var dateTime = require('node-datetime');
 
 const client = new snoowrap(credentials.info);
 
+var filepath = "./recent.txt";
+
 var run = function() {
+  var dt = dateTime.create();
+  console.log(dt.format('Y-m-d H:M:S') + " - [Running]");
   var inbox = client.getInbox("messages");
 
   inbox.then(function(result) {
     var postCreated = false;
     for (i = 0; i < result.length && postCreated == false; i++) {
-      var message = checkMessage(result[i])
+      var message = checkMessage(result[i]);
       if (message != null) {
 	var link = getGTLink(message.body);
 	postCreated = true;
 	makePost(link);
       }
     }
-    run(); // TODO - Add sleep before run, otherwise will constantly send requests to Reddit 
+    setTimeout(run, 60000 * 5); // 1 minute == 60000
   })
 }
 
 // If message is from hockey bot and has not been posted already
 // TODO - check to see if this Game Thread has been posted already
 var checkMessage = function(comment) {
+ // console.log(comment);
+  if (comment.author == null) { return null; }
   var author = comment.author.name;
-  
-  if (author.includes("HockeyGT_Bot")) {
+  var currTime = parseInt(comment.created);
+  // Gets int from file containing time from last posted file
+  var lastTime = parseInt(fs.readFileSync(filepath).toString().replace(/\s+/g, ''));
+ // console.log(lastTime);
+  if (author.includes("HockeyGT_Bot") && currTime > lastTime) {
+    fs.writeFileSync(filepath, currTime.toString());
     return comment; 
   }
   return null;
 }
 
-// TODO - Makes post to subreddit 
+// Makes post to subreddit 
 var makePost = function(link) {
-  console.log(link);
+  var dt = dateTime.create();
+  var date = dt.format('d/m/Y');
+  var title = "Game Thread - " + date;
+  var body = "Welcome to the Game Thread for " + date + "\n\n" +
+             "[Follow this link to the /r/hockey game thread.]("
+             + link + ")\n\n"; 
+  console.log(body);
+  client.getSubreddit('valentine96_hocky').submitSelfpost({title: title, text: body}).then(console.log);
 }
 
 // Parse game thread link from message body, returns null if link does not exist. 
